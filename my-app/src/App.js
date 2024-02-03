a774180e04c15e69805c37b587e04e8cf392beff
RohanShah10 <rohanshah.13@hotmail.co.uk>
2024-02-03 16:48:07 +0000

    Added basic styling to the table

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const students = [
+    {
+      id: "0101102",
+      name: "Rohan Shah",
+      lessonsCompleted: 15,
+      testsCompleted: 5,
+      testsMissed: 2,
+      averageMark: "75%",
+    },
+    {
+      id: "0101103",
+      name: "Aaryan Purohit",
+      lessonsCompleted: 9,
+      testsCompleted: 1,
+      testsMissed: 6,
+      averageMark: "35%",
+    },
+    {
+      id: "0101104",
+      name: "Thalaiyur Mohammad Rayees Muhamad Mohsin",
+      lessonsCompleted: 3,
+      testsCompleted: 2,
+      testsMissed: 5,
+      averageMark: "95%",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Student ID Number</th>
+            <th>Student Name</th>
+            <th>Lessons Completed</th>
+            <th>Tests Completed</th>
+            <th>Tests Missed</th>
+            <th>Average Test Mark</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student) => (
+            <tr key={student.id}>
+              <td>{student.id}</td>
+              <td>{student.name}</td>
+              <td>{student.lessonsCompleted}</td>
+              <td>{student.testsCompleted}</td>
+              <td>{student.testsMissed}</td>
+              <td>{student.averageMark}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
